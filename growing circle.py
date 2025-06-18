a3fd2da4269ef08a18eebef1a8a2ec18d449cc3a
aahilsalman999 <aahilsalman1@hotmail.com>
2025-06-18 17:18:18 +0100

    Add files via upload

--- growing circle.py
+++ growing circle.py
@@ -19,16 +19,18 @@ class Circle:
         self.radius = self.radius + in_radius
         self.draw_circle = pygame.draw.circle(self.surface,self.color,self.pos,self.radius,self.width)
 
-
-circle1 = Circle(Blue,(0,0),30,30)
+circle1 = Circle(Blue,(400,300),30,0)
 while True:
     for event in pygame.event.get():
         if event.type == pygame.MOUSEBUTTONDOWN:
             SCREEN.fill((255,255,255))
             circle1.draw()
             pygame.display.update()
-        
-
+        elif event.type == pygame.MOUSEBUTTONUP:
+            SCREEN.fill((255,255,255))
+            circle1.grow(20)
+            pygame.display.update()
+            
 
 
 
