c1e8f91f60a3d7381bdc8ac388d31b129d3c259e
aahilsalman999 <aahilsalman1@hotmail.com>
2025-09-10 17:19:36 +0100

    Add files via upload

--- recycle.py
+++ recycle.py
@@ -1,107 +1,128 @@
-import pgzrun
+import pygame
 import random
-font_option = (255,255,255)
-width = 800
-height = 600
-centre_x = width / 2
-centre_y = height / 2
-centre = (centre_x , centre_y)
-final_level = 6
-start_speed = 20
-ITEMS = ["rugby","ball"]
-game_over = False
-game_complete = False
-items = []
-animations = []
-current_level =1
+import time
+from pygame.locals import *
+pygame.init()
+pygame.display.set_caption("Recycle loot!")
+WIDTH = 700
+HEIGHT = 600
+SCREEN = pygame.display.set_mode((WIDTH,HEIGHT))
 
-def draw():
-    global items , current_level , game_over , game_complete
-    screen.clear()
-    screen.blit("backround" , (0,0))
-    if game_over:
-        display_message("Game 0ver!" , "Try again!")
-    elif game_complete:
-        display_message("You Won!" , "Well done!")
+def change_background(image):
+    background = pygame.image.load(image)
+    img = pygame.transform.scale(background,(WIDTH,HEIGHT))
+    SCREEN.blit(img,(0,0))
+
+class Bin(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.bin = pygame.image.load("img/bin.png").convert_alpha()
+        self.bin = pygame.transform.scale(self.bin,(60,80))
+        self.rect = self.bin.get_rect()
+
+class Recycable(pygame.sprite.Sprite):
+    def __init__(self,img):
+        super().__init__()
+        self.recycable = pygame.image.load(img).convert_alpha()
+        self.recycable = pygame.transform.scale(self.recycable,(60,60))
+        self.rect = self.recycable.get_rect()
+
+class Non_recycable(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.non_recycable = pygame.image.load("img/batteries.png").convert_alpha()
+        self.non_recycable = pygame.transform.scale(self.non_recycable,(60,60))
+        self.rect = self.non_recycable.get_rect()
+
+images = ["img/bags.png","img/box.png","img/newspaper.png","img/pencil.png"]
+
+#create sprite groups
+items_list = pygame.sprite.Group()
+battery_list = pygame.sprite.Group()
+all_sprite_list = pygame.sprite.Group()
+
+#create recycable items sprite
+for sprite in range(35):
+    item = Recycable(random.choice(images))
+    item.rect.x = random.randrange(WIDTH - 100)
+    item.rect.y = random.randrange(HEIGHT - 100)
+    items_list.add(item)
+    all_sprite_list.add(item)
+
+#create non recycable items sprite
+for sprite in range(20):
+    item = Non_recycable()
+    item.rect.x = random.randrange(WIDTH - 100)
+    item.rect.y = random.randrange(HEIGHT - 100)
+    battery_list.add(item)
+    all_sprite_list.add(item)
+
+#create bin sprite
+bin = Bin()
+all_sprite_list.add(bin)
+
+Red = (255,0,0)
+Green = (0,255,0)
+Blue = (0,0,255)
+White = (255,255,255)
+
+running = True
+score_count = 0
+clock = pygame.time.Clock()
+start_time = time.time()
+
+time_font = pygame.font.SysFont("Comicsansms",30)
+my_font = pygame.font.SysFont("Comicsansms",35)
+message_font = pygame.font.SysFont("Comicsansms",50)
+text = my_font.render("Score: "+ str(score_count),True,White)
+
+while running:
+    clock.tick(30)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    time_elapsed = time.time() - start_time
+    if time_elapsed >= 50:
+        if score_count > 100 and len(battery_list) >= 15:
+            text = message_font.render("Congrats! You won!",True,Green)
+            change_background("img/good loot.jpg")
+        else:
+            text = message_font.render("Unlucky! You lost!",True,Red)
+            change_background("img/bad loot.png")
     else:
-        for item in items:
-           item.draw()
-
-def update():
-    global items
-    if len(items) == 0:
-        items = make_items(current_level)
-
-def make_items(number_of_extra_items):
-    items_to_create = get_option_to_create(number_of_extra_items)
-    new_items = create_items(items_to_create)
-    layout_items(new_items)
-    animate_items(new_items)
-    return new_items
-
-def get_option_to_create(number_of_extra_items):
-     items_to_create = ["cricket"]
-     for i in range(0,number_of_extra_items):
-        random_option = random.choice(ITEMS)
-        items_to_create.append(random_option)
-     return items_to_create
-
-def create_items(items_to_create):
-     new_items = []
-     for option in items_to_create:
-         item = Actor(option)
-         new_items.append(item)
-     return new_items
-
-def layout_items(items_to_layout):
-     number_of_gaps = len(items_to_layout) + 1
-     gap_size = width / number_of_gaps
-     random.shuffle(items_to_layout)
-     for index,item in enumerate(items_to_layout):
-       new_x_pos = (index + 1) * gap_size
-       item.x = new_x_pos
+        change_background("img/bg.jpg")
+        countdown = time_font.render("Time left:"+ str(50 - int(time_elapsed)),True,White)
+        SCREEN.blit(countdown,(350,300))
     
-def animate_items(items_to_animate):
-     global animations
-     for item in items_to_animate:
-         duration = start_speed - current_level
-         item.anchor = ("center","bottom")
-         animation = animate(item,duration = duration, on_finished = handle_game_over , y = height)
-         animations.append(animation)
+        keys = pygame.key.get_pressed() 
+        if keys[pygame.K_UP]:
+            if bin.rect.y > 0:
+                bin.rect.y -= 5
+        if keys[pygame.K_DOWN]:
+            if bin.rect.y < 530:
+                bin.rect.y += 5
+        if keys[pygame.K_RIGHT]:
+            if bin.rect.x < 650:
+                bin.rect.x += 5
+        if keys[pygame.K_LEFT]:
+            if bin.rect > 0:
+                bin.rect.x -= 5
 
-def handle_game_over():
-    global game_over
-    game_over = True
+        item_hit_list = pygame.sprite.spritecollide(bin,items_list,True)
+        battery_hit_list = pygame.sprite.spritecollide(bin,battery_list,True)
+        
+        for item in item_hit_list:
+            score_count += 5
+            text = my_font.render("Score: "+str(score_count),True,White)
+        for battery in battery_hit_list:
+            score_count -= 5
+            text = my_font.render("Score: "+str(score_count),True,White)
+        
+        SCREEN.blit(text,(20,50))
 
-def on_mouse_down(pos):
-    global items , current_level
-    for item in items:
-        if item.collidepoint(pos):
-            if "cricket" in item.image:
-                handle_game_complete()
-            else:
-                handle_game_over()
-
-def handle_game_complete():
-    global current_level , items , animations , game_complete
-    stop_animations(animations)
-    if current_level == final_level:
-        game_complete = True
-    else:
-        current_level += 1
-        items = []
-        animations = []
-
-def stop_animations(animations_to_stop):
-    for animation in animations_to_stop:
-        if animation.running:
-            animation.stop()
-
-def display_message(heading_text , subheading_text):
-    screen.draw.text(heading_text , fontsize = 60 , center = centre , color = "black")
-    screen.draw.text(subheading_text , fontsize = 30 , center = (centre_x , centre_y + 30) , color = "black")
-pgzrun.go()
-
-
+        all_sprite_list.draw(SCREEN)
+    pygame.display.update()
+pygame.quit()
 
 
